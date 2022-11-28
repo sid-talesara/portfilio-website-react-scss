6721e7d4d312156cdac6c48b591ecd0ed7773c8f
Siddharth Talesara <siddharthtalesara01@gmail.com>
2022-11-28 17:47:12 +0530

    commit 2.5 Implemented framer in AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,23 +1,48 @@
 import React from "react";
 import home1 from "../img/home1.png";
+// framer motion
+import { motion } from "framer-motion";
+
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+
+    show: { opacity: 1, transition: { duration: 2, when: "afterChildren" } },
+  };
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 0.75,
+        staggerChildren: 0.75,
+      },
+    },
+  };
+
   return (
     <div className="about-section">
       <div className="description">
-        <div className="title">
+        <motion.div
+          className="title"
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
           <div className="hide">
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </div>
           <div className="hide">
-            <h2>
+            <motion.h2 variants={titleAnim}>
               {" "}
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </div>
           <div className="hide">
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </div>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas to discuss, we
           have professionals with amazing skills.
